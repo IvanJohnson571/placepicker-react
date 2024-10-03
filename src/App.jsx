a7960d64c37e9a4b6fccede3851cbac0bc4cf38a
IvanJohnson571 <ivan_johnson2005@yahoo.com>
2024-10-03 13:24:46 +0300

    finalize setting data to localStorage.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 function App() {
 
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
 
@@ -31,6 +36,11 @@ function App() {
   function handleStartRemovePlace(id) {
     modal.current.open();
     selectedPlace.current = id;
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
+
   }
 
   function handleStopRemovePlace() {
